7d7aac08715cbb7b3e862b4acea7039889206d4f
Seth Itow <thedingwing@gmail.com>
2015-11-07 18:29:39 -0800

    added body of function added getParam function

--- scripts/kill.js
+++ scripts/kill.js
@@ -1,5 +1,22 @@
 // this function will get called by the killer client 
 // it will change the 
-function processKill(killerID, killeeID){
+// killerID will be the user's ID
+// the killee will be the scanned ID
 
+
+function processKill(killer, killee){
+var GROUP_ID = getParam("group");
+
+fb.child(GROUP_ID).child(players).child(killee).child(alive).set("false"); // change status of killee
+var killeeTarget = fb.child(GROUP_ID).child(players).child(killee).child(target); // get target of killee
+fb.child(GROUP_ID).child(players).child(killee).child(killlee).set(killeeTarget); // update target of killer
+
+}
+
+function getParam(name) {
+	
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
