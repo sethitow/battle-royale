05033f8dd6a7fea9700683ea746aed2bffa41172
Aashray Anand <Aashray_anand@hotmail.com>
2015-11-07 18:35:51 -0800

    qr changes

--- scripts/goqr.js
+++ scripts/goqr.js
@@ -1,8 +1,9 @@
-$('#Run').click(function ()
-{
+$('#Run').click(function (){
     console.log("hello");
-    var x = 'http://api.qrserver.com/v1/create-qr-code/?data=[URL-encoded-text]&size=[150]x[150]';
-    $http.get(x).then(function (response){
-        
-    })
+    var b = $('#Content').val();
+    var x = 'http://api.qrserver.com/v1/create-qr-code/?data=[' + b + ']&size=[150]x[150]';
+    console.log(x);
+    document.getElementById("results").src = x;
+    $http.get(x).then(function (response) {
+    });
 });
