0620331d69d7f3bc9747ecfe6006a6bec866f375
Seth Itow <thedingwing@gmail.com>
2015-11-08 08:12:53 -0800

    sort of working

--- server/relay.php
+++ server/relay.php
@@ -1,14 +1,20 @@
 <?php
 
 header('Access-Control-Allow-Origin: *');
-$postData = file_get_contents('php://input');
 
 $url = 'http://api.qrserver.com/v1/read-qr-code/';
 $ch = curl_init($url);
 
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+curl_setopt(
+    $request,
+    CURLOPT_POSTFIELDS,
+    array(
+      'file' =>
+          '@'            . $_FILES['file']['tmp_name']
+          . ';filename=' . $_FILES['file']['name']
+    ));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec($ch);
 echo $response;
