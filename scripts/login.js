f3022742ce1f43778bf8c01e4a21f1d1459245c0
AaronJohnston <aaron.johnston001@gmail.com>
2015-11-07 18:14:47 -0800

    added index.html content for Peter

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,20 @@
-$(document).ready(function () {
+var action = "";
 
+function openLogin(currentAction) {
+    action = currentAction;
+    $("#loginBox").slideDown();
+}
+
+$(document).ready(function() {
+    
+    $("#loginBox").slideUp(0);
+
+    $("#create").click(function() {
+        openLogin("create");
+    });
+    
+    $("#join").click(function() {
+        openLogin("join");
+    });
 
 }
