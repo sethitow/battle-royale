668169d3b0bd7b9876999f03c5ac6ec73a1531c6
Seth Itow <thedingwing@gmail.com>
2015-11-09 11:53:54 -0800

    tried different relay server approach (still doesn't work)

--- server/relay.php
+++ server/relay.php
@@ -5,15 +5,10 @@ header('Access-Control-Allow-Origin: *');
 $url = 'http://api.qrserver.com/v1/read-qr-code/';
 $ch = curl_init($url);
 
+echo $HTTP_RAW_POST_DATA;
+
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt(
-    $request,
-    CURLOPT_POSTFIELDS,
-    array(
-      'file' =>
-          '@'            . $_FILES['file']['tmp_name']
-          . ';filename=' . $_FILES['file']['name']
-    ));
+curl_setopt($ch, CURLOPT_POSTFIELDS, $HTTP_RAW_POST_DATA);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
 $response = curl_exec($ch);
